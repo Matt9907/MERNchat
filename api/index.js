4aedaa77b28387827706f143b4eb0b429ab465e6
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-07-27 14:00:47 -0400

    Setting up API calls for Register

--- api/index.js
+++ api/index.js
@@ -1,21 +1,50 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const jsonToken = require('jsonwebtoken');
+const cors = require('cors');
+const User = require('./models/User');
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
+    
+const jsonSecret = process.env.JSON_SECRET;
 
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials:true,
+    origin: process.env.CLIENT_URL,
+}));
 
 app.get('/test', (req,res) =>{
     res.json('test ok');
 });
 
-app.post('/register', (req,res) =>{
+app.post('/register', async (req,res) =>{
+    const {username, password} = req.body;
+    try{
+        const createUser = await User.create({username, password});
+        jsonToken.sign({userId: createUser, _id}, jsonSecret,{}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json('ok');
+        });
 
+    }catch(err){
+        if(err) throw err;
+        res.status(500).json('error');
 
+    }
 });
+    
+
+    
+ 
+
+
+
+
 
 
 app.listen(4000);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import Register from "./Register"
+import axios from "axios";
 
 
 function App() {
+  axios.defaults.baseURL = " http://localhost:5173"
+  axios.defaults.withCredentials = true;
   
 
   return (
