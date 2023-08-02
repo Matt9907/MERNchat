e9c3afee65d447c3c72ad358ea0d51a49fcedf64
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-08-01 22:06:19 -0400

    Setting up frontend for Login page

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -17,16 +17,37 @@ export default function Register(){
     }
     return(
         <div className="bg-blue-50 h-screen flex items-center">
-            <form className="w-64 mx-auto mb-12" onSubmit={register}>
-                <input value={username} 
+        <form className="w-64 mx-auto mb-12" onSubmit={register}>
+        <input value={username} 
                 onChange={ev => setUsername(ev.target.value)} 
                 type ="text" placeholder="username" 
                 className="block w-full rounded-sm p-2 mb-2 border" />
-                <input value={password} 
+        <input value={password} 
                 onChange={ev => setPassword(ev.target.value)} 
                 type = "password" placeholder="password" 
                 className="block w-full rounded-sm p-2 mb-2 border" />
-                <button className="bg-blue-500 text-white block w-full rounded-sm p-2">Register</button>
+        <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+                    {isLoginOrRegister ==='register' ? 'Register' : 'Login'}
+                    </button>
+        <div className="text-center mt-2">   
+        {isLoginOrRegister === 'register' && (
+            <div>
+                 Already a member? 
+        <button className='m1-1' onClick={() => setIsLoginOrRegister('login')}>
+                    Login here
+                    </button> 
+                
+                </div>
+        )}
+        {isLoginOrRegister === 'register' && (
+            <div>
+                Dont have an account?
+                <button className = 'm1-1' onClick={() => setIsLoginOrRegister('register')}>
+                    Register
+                </button>
+            </div>
+        )}
+        </div>
             </form>
         </div>
     )
