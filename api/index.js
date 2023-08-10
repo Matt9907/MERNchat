09c07da961c7e32675ccd02ee2f1dcc2f8c1129a
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-08-10 11:46:22 -0400

    Styling and Showing Users Online

--- api/index.js
+++ api/index.js
@@ -114,6 +114,13 @@ wss.on('connection',(connection, req) =>{
     }
  }    
 
+[...wss.clients].forEach(client => {
+    client.send(JSON.stringify({
+      online:  [...wss.clients].map(c => ({userId:c.userId,username:c.username}))
+    }))
+});
+
+
 });
 
 
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,19 +1,47 @@
 export default function Chat(){
     const [ws, setWs] = useState(null);
+    const[onlinePeople,setOnlinePeople] = useState({});
 useEffect(() =>{
     const ws = new WebSocket('ws://localhost:5173');
     setWs(ws);
     ws.addEventListener('message',handleMessage )
 } ,[]);
-function handleMessage(e){
-    console.log('new message', e);
+
+function showOnlinePeople(peopleArray){
+    const people = {};
+    peopleArray.forEach(({userId,username}) => {
+        people[userId] = username;
+    });
+    setOnlinePeople(people);
+
+    }
+        
+  
+
+
+
+
+
+
+function handleMessage(ev){
+    const messageData = JSON.parse(ev.data);
+    if('online' in messageData){
+        showOnlinePeople(messageData.online);
+    }
 
 }
 
     return(
         <div className="flex h-screen">
             <div className = "bg-white w-1/3 flex-col">
-                Contacts
+                <div className="text-blue-600 font-bold">
+                    MernChat
+                    </div>
+                {Object.keys(onlinePeople).map(userId =>(
+                    <div className="border-b border-gray-100 py-2">
+                        {onlinePeople[userId]}
+                        </div>
+                ))}
             </div>
             <div className="flex flex-col bg-blue-50 w-2/3 p-2">
                 <div className="flex-grow"> 
